005bc9f0c74cd438cbb7d1061a3c2dea786d5ae7
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2019-02-18 19:12:39 +0100

    Aprobado conseguido, events.js created and exports EventEmitter class

--- events.js
+++ events.js
@@ -1,7 +1,16 @@
 
 
+class EventEmitter {
+
+    emit(){
+        console.log('emitter method');
+    }
+    on(){
+        console.log('listener method');
+    }
+}
 
 
 
 
-exports = module.exports = Termostato;
+exports = module.exports = EventEmmitter;
